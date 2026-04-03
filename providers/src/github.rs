5ba364d3e211b1d9ebefeb9e8a617dc4142034b6
d <dminnear.alt@gmail.com>
2026-04-03 00:34:02 -0400

    implementing webhook parsing for prs

--- providers/src/github.rs
+++ providers/src/github.rs
@@ -1,17 +1,40 @@
-use std::sync::Arc;
+use std::{fmt::Debug, sync::Arc};
 
 use app_config::AppConfig;
 use axum::{
   body::Bytes,
   extract::State,
   http::{HeaderMap, StatusCode},
-  response::IntoResponse,
 };
 use hmac::{Hmac, KeyInit, Mac};
+use jsonwebtoken::EncodingKey;
+use octocrab::{
+  Octocrab,
+  models::webhook_events::{
+    WebhookEvent, WebhookEventPayload, payload::PullRequestWebhookEventAction,
+  },
+};
+use serde::{Deserialize, de::Error};
 use sha2::Sha256;
+use thiserror::Error;
 use tracing::{info, warn};
 
 use super::get_header;
+
+const GITHUB_EVENT_PUSH: &str = "push";
+const GITHUB_EVENT_PULL_REQUEST: &str = "pull_request";
+
+#[derive(Debug, Error)]
+pub enum GithubError {
+  #[error("Octocrab error: {0}")]
+  Octocrab(#[from] octocrab::Error),
+  #[error("Invalid GitHub App ID: {0}")]
+  InvalidAppId(#[from] std::num::ParseIntError),
+  #[error("Invalid GitHub App private key: {0}")]
+  InvalidPrivateKey(#[from] jsonwebtoken::errors::Error),
+  #[error("Invalid webhook event payload: {0}")]
+  InvalidPayload(#[from] serde_json::Error),
+}
 
 pub struct GithubProvider;
 
@@ -20,30 +43,104 @@ impl GithubProvider {
     State(config): State<Arc<AppConfig>>,
     headers: HeaderMap,
     body: Bytes,
-  ) -> impl IntoResponse {
+  ) -> StatusCode {
     let signature = get_header(&headers, "X-Hub-Signature-256");
-
     if !signature_matches(&body, &signature, config.github_webhook_secret()) {
       warn!("Unauthorized webhook attempt: Signature mismatch");
       return StatusCode::UNAUTHORIZED;
     }
 
     let event_type = get_header(&headers, "X-GitHub-Event");
-    info!("Verified GitHub event received: {}", event_type);
-
-    let json_result: Result<serde_json::Value, _> = serde_json::from_slice(&body);
-
-    match json_result {
-      Ok(payload) => {
-        process_event(&event_type, payload);
-        StatusCode::ACCEPTED
-      }
-      Err(e) => {
-        warn!("Failed to parse verified webhook JSON: {}", e);
-        StatusCode::BAD_REQUEST
+    match event_type.as_str() {
+      GITHUB_EVENT_PUSH => match handle_push(&body, config).await {
+        Ok(status) => status,
+        Err(e) => {
+          warn!(error = ?e, "Failed to handle push event");
+          StatusCode::INTERNAL_SERVER_ERROR
+        }
+      },
+      GITHUB_EVENT_PULL_REQUEST => match handle_pull_request(&body, config).await {
+        Ok(status) => status,
+        Err(e) => {
+          warn!(error = ?e, "Failed to handle pull request event");
+          StatusCode::INTERNAL_SERVER_ERROR
+        }
+      },
+      _ => {
+        info!(event_type, "Received unsupported GitHub event");
+        StatusCode::NOT_IMPLEMENTED
       }
     }
   }
+}
+
+async fn handle_push(payload: &[u8], config: Arc<AppConfig>) -> Result<StatusCode, GithubError> {
+  Ok(StatusCode::NOT_IMPLEMENTED)
+}
+
+async fn handle_pull_request(
+  payload: &[u8],
+  config: Arc<AppConfig>,
+) -> Result<StatusCode, GithubError> {
+  let event = WebhookEvent::try_from_header_and_body(GITHUB_EVENT_PULL_REQUEST, payload)?;
+  let WebhookEventPayload::PullRequest(pr_event) = event.specific else {
+    return Err(GithubError::InvalidPayload(serde_json::Error::custom(
+      "Bad pull request event payload",
+    )));
+  };
+
+  if pr_event.action != PullRequestWebhookEventAction::Opened
+    && pr_event.action != PullRequestWebhookEventAction::Synchronize
+  {
+    info!(action = ?pr_event.action, "Ignoring pull request event with unsupported action");
+    return Ok(StatusCode::NOT_IMPLEMENTED);
+  }
+
+  let pr = pr_event.pull_request;
+  let owner = pr
+    .repo
+    .clone()
+    .ok_or_else(|| {
+      GithubError::InvalidPayload(serde_json::Error::custom(
+        "Missing repository information in pull request event",
+      ))
+    })?
+    .owner
+    .ok_or_else(|| {
+      GithubError::InvalidPayload(serde_json::Error::custom(
+        "Missing owner information in pull request event",
+      ))
+    })?
+    .login;
+  let repo = pr
+    .repo
+    .ok_or_else(|| {
+      GithubError::InvalidPayload(serde_json::Error::custom(
+        "Missing repository information in pull request event",
+      ))
+    })?
+    .name;
+  let sha = pr.head.sha;
+  let ref_field = pr.head.ref_field;
+  start_pr_pipelines(&owner, &repo, &sha, &ref_field, config).await?;
+  Ok(StatusCode::OK)
+}
+
+async fn start_pr_pipelines(
+  owner: &str,
+  repo: &str,
+  sha: &str,
+  ref_field: &str,
+  config: Arc<AppConfig>,
+) -> Result<(), GithubError> {
+  let crab = Octocrab::builder()
+    .app(
+      config.github_app_id().parse::<u64>()?.into(),
+      EncodingKey::from_rsa_pem(config.github_private_key().as_bytes())?,
+    )
+    .build()?;
+
+  Ok(())
 }
 
 fn signature_matches(payload: &[u8], signature_header: &str, secret: &str) -> bool {
@@ -61,13 +158,4 @@ fn signature_matches(payload: &[u8], signature_header: &str, secret: &str) -> bo
 
   mac.update(payload);
   mac.verify_slice(&expected_signature).is_ok()
-}
-
-fn process_event(event_type: &str, _payload: serde_json::Value) {
-  match event_type {
-    "push" => {
-      info!("Push event received. Time to find the .jefferies folder!");
-    }
-    _ => info!("Received unhandled event type: {}", event_type),
-  }
 }
